9c31b415afe9b3bfdec1c7c0456a3390ed4c3869
Katrina MacGregor <katmacgregor@gmail.com>
2016-01-17 12:46:08 -0800

    Add querying for time settings

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,23 +5,46 @@ $(document).ready(function() {
 
   $('.main-container').addClass('loaded');
 
+  function setDay(){
+    body.addClass('day');
+  }
+
+  function setAfternoon(){
+    body.addClass('afternoon');
+    setDay();
+  }
+
+  function setNight(){
+    body.addClass('night');
+  }
+
+  function setEvening(){
+    body.addClass('evening');
+    setNight();
+  }
+
+  function setMidnight(){
+    body.addClass('midnight');
+    setNight();
+  }
+
   function dayNight(){
     var currentTime = new Date().getHours();
 
     if ((currentTime >= 0) && (currentTime < 5)) {
-      body.addClass('night midnight');
+      setMidnight();
     }
     if ((currentTime >= 5) && (currentTime < 12)) {
-      body.addClass('day');
+      setDay();
     }
     if ((currentTime >= 12) && (currentTime < 16)) {
-      body.addClass('day afternoon');
+      setAfternoon();
     }
     if ((currentTime >= 16) && (currentTime < 20)) {
-      body.addClass('night evening');
+      setEvening();
     }
     if ((currentTime >= 20 ) && (currentTime < 24)) {
-      body.addClass('night');
+      setNight();
     }
   }
 
@@ -41,4 +64,31 @@ $(document).ready(function() {
     container.removeClass('active');
     $(this).removeClass('visible');
   });
+
+
+  // Query Time of Day
+  var currHref = window.location.href,
+      hasQuery = currHref.indexOf('=') !== -1,
+      query = hasQuery && currHref.split('=')[1];
+
+  if(hasQuery){
+    body.removeClass('night day afternoon evening midnight');
+
+    switch(query){
+      case 'night':
+        setNight();
+        break;
+      case 'evening':
+        setEvening();
+        break;
+      case 'midnight':
+        setMidnight();
+        break;
+      case 'afternoon':
+        setAfternoon();
+        break;
+      default:
+        setDay();
+    }
+  }
 });
